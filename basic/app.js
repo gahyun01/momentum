a13c869c80c1c53b75d5336c17137863e67e5d1d
gahyun <gahyun727301@kangnam.ac.kr>
2024-12-19 10:01:51 +0900

    # 2.7 Functions part One &&  # 2.8 Functions part Two

--- basic/app.js
+++ basic/app.js
@@ -60,3 +60,46 @@ player.fat = false;
 player.lastName = "potato";    // 새로운 key를 추가함 ( lastName )
 player.points = player.points + 15;
 console.log(player);
+
+
+
+// 2.7 Functions part One && 2.8 Functions part Two
+function sayHello(nameOfPerson, age) {
+    // console.log("Hello!");
+    // console.log(nameOfPerson);
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+// sayHello();
+console.log("hello");   // 함수에 데이터를 보내려면 ()안에 데이터를 넣어줘야함.
+
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+
+// function plus(a, b){
+//     console.log(a + b);
+// }
+// plus();    // NaN (Not a Number) - 값이 없음. <- a와 b에 값을 넣어줘야함.
+// plus(8, 60);
+
+
+// simple calculator2
+function plus(firstNumber, secondNumber){
+    console.log(firstNumber + secondNumber);
+}
+function divide(a, b){
+    console.log(a / b);
+}
+// plus(8, 60);    // firstNumber == 8, secondNumber == 60
+plus(60, 8);    // firstNumber == 60, secondNumber == 8
+divide(98, 20);
+
+const player1 = {
+    name: "nico",
+    sayHello: function(otherPersonName){
+        console.log("hello " + otherPersonName + " nice to meet you!");
+    },
+};
+
+console.log(player1.name);
+player1.sayHello("lynn");
